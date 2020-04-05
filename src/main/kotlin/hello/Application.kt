5a3cedb72871591290300be3e2d9d8c3f9339cd6
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-06 06:05:20 +0700

    #17 - Add  id("org.jetbrains.kotlin.plugin.jpa") version "1.3.71"

--- src/main/kotlin/hello/Application.kt
+++ src/main/kotlin/hello/Application.kt
@@ -20,28 +20,28 @@ class Application {
 			repository.save(Customer("Sakura", "Haruno"))
 			repository.save(Customer("Hatake", "Kakashi"))
 
-			// fetch all customers
-			log.info("Customers found with findAll():")
-			log.info("-------------------------------")
-			for (customer in repository.findAll()) {
-				log.info(customer.toString())
-			}
-			log.info("")
-
-			// fetch an individual customer by ID
-			val customer = repository.findOne(1L)
-			log.info("Customer found with findOne(1L):")
-			log.info("--------------------------------")
+		// fetch all customers
+		log.info("Customers found with findAll():")
+		log.info("-------------------------------")
+		for (customer in repository.findAll()) {
 			log.info(customer.toString())
-			log.info("")
+		}
+		log.info("")
 
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):")
-			log.info("--------------------------------------------")
-			for (bauer in repository.findByLastName("Bauer")) {
-				log.info(bauer.toString())
-			}
-			log.info("")
+		// fetch an individual customer by ID
+		val customer = repository.findById(1L)
+		log.info("Customer found with findById(1L):")
+		log.info("--------------------------------")
+		log.info(customer.toString())
+		log.info("")
+
+		// fetch customers by last name
+		log.info("Customer found with findByLastName('Naruto'):")
+		log.info("--------------------------------------------")
+		for (Naruto in repository.findByLastName("Naruto")) {
+			log.info(Naruto.toString())
+		}
+		log.info("")
 	}
 
 }
