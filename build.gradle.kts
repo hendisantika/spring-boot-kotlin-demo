9255fef651618be24d03dfb721e274409e679dd5
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-06 06:00:49 +0700

    #16 - Use latest gradle script & gradle wrapper version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,39 @@
-buildscript {
-	val springBootVersion = "1.5.22.RELEASE"
-    val kotlinVersion = "1.2.71"
-	extra["kotlinVersion"] = kotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-	repositories {
-		mavenCentral()
-	}
-
-	dependencies {
-		classpath("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
-		classpath("org.jetbrains.kotlin:kotlin-noarg:$kotlinVersion")
-		classpath("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-	}
+plugins {
+	id("org.springframework.boot") version "2.2.6.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	kotlin("jvm") version "1.3.71"
+	kotlin("plugin.spring") version "1.3.71"
 }
 
-apply {
-	plugin("kotlin")
-	plugin("kotlin-spring")
-	plugin("kotlin-jpa")
-	plugin("org.springframework.boot")
-}
-
+group = "com.example"
 version = "0.0.1-SNAPSHOT"
-
-configure<JavaPluginConvention> {
-	setSourceCompatibility(1.8)
-	setTargetCompatibility(1.8)
-}
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
 	mavenCentral()
 }
 
-val kotlinVersion = extra["kotlinVersion"] as String
-
 dependencies {
-	compile("org.springframework.boot:spring-boot-starter-web")
-	compile("org.springframework.boot:spring-boot-starter-data-jpa")
-	compile("com.h2database:h2")
-	compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-	compile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-	compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.8.4")
-	testCompile("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("com.h2database:h2")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
 }
 
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
