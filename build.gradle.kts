dcd58327380fe35cc6463771206126ffadf81e00
Hendi Santika <hendisantika@yahoo.co.id>
2024-09-09 18:26:43 +0700

    Upgraded into Spring Boot 3.3.1 version 🫸🌀✏️📗🐧🐳⬆
    Upgraded into Spring Boot 3.3.0 version 🫸🌀✏️📗🐧🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.3.0"
+	id("org.springframework.boot") version "3.3.1"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	kotlin("jvm") version "1.3.71"
 	kotlin("plugin.spring") version "1.3.71"
@@ -35,6 +35,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+		jvmTarget = "21"
 	}
 }
